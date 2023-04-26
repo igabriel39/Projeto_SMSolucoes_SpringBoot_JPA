75df810b3de8e50fe62f3bcfb0e1a7e192ffbdf6
igabriel39 <gabriel.mendonca2@hotmail.com>
2023-04-26 14:34:46 -0300

    CRUD entidade Cliente

--- src/main/java/com/example/SmSolucoes/controller/ClienteController.java
+++ src/main/java/com/example/SmSolucoes/controller/ClienteController.java
@@ -2,22 +2,37 @@ package com.example.SmSolucoes.controller;
 
 import com.example.SmSolucoes.model.Cliente;
 import com.example.SmSolucoes.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
-    @Autowired
     private ClienteRepository clienteRepository;
 
     @GetMapping
-    public List<Cliente> consultar() {
+    public List<Cliente> getClientes() {
         return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente getClienteById(@PathVariable Integer id) {
+        return clienteRepository.findById(id).get();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente saveCliente(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCliente(@PathVariable Integer id) {
+        clienteRepository.deleteById(id);
     }
 }
--- src/main/java/com/example/SmSolucoes/model/Cliente.java
+++ src/main/java/com/example/SmSolucoes/model/Cliente.java
@@ -1,8 +1,12 @@
 package com.example.SmSolucoes.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 public class Cliente {
