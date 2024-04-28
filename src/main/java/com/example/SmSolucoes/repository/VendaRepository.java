3876eb01f4d67a6c17bbb497250393c5e246536c
igabriel39 <gabriel.mendonca2@hotmail.com>
2024-04-28 12:29:14 -0300

    criação interface para retornar consulta das vendas

--- src/main/java/com/example/SmSolucoes/repository/VendaRepository.java
+++ src/main/java/com/example/SmSolucoes/repository/VendaRepository.java
@@ -4,7 +4,6 @@ import com.example.SmSolucoes.model.VendaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.w3c.dom.stylesheets.LinkStyle;
 
 import java.util.List;
 
